40bd31e4d4cad3597187296a3dbe0ac2427a08b4
asifmak2023 <160400379+asifmak2023@users.noreply.github.com>
2024-03-02 20:53:49 +0500

    Update main.ts

--- Program42/main.ts
+++ Program42/main.ts
@@ -1,25 +1,25 @@
 /*
-Great Magicians: Start with a copy of your program from Exercise 39. 
-Write a function called make_great() that modifies the array of magicians 
-by adding the phrase the Great to each magician’s name. 
+Great Magicians: Start with a copy of your program from Exercise 39.
+Write a function called make_great() that modifies the array of magicians
+by adding the phrase the Great to each magician’s name.
 Call show_magicians() to see that the list has actually been modified.
 */
 console.clear();
 //Create function
-function show_magician(magicians : string[]): void {
-  for (const magician of magicians) {
+function show_magician(magicians) {
+    for (var _i = 0, magicians_1 = magicians; _i < magicians_1.length; _i++) {
+        var magician = magicians_1[_i];
         console.log(magician);
     }
 }
-
-function make_great(magicians : string[]): void {
-  for (const magician of magicians) {
-        console.log(`Great ${magician}`);
+function make_great(magicians) {
+    for (var _i = 0, magicians_2 = magicians; _i < magicians_2.length; _i++) {
+        var magician = magicians_2[_i];
+        console.log("Great ".concat(magician));
     }
 }
 //Create list of Magician's names
-const magician_names : string[] = ["Magician1", "Magician2", "Magician3"];
-
+var magician_names = ["Magician1", "Magician2", "Magician3"];
 //call the function
-make_great(magician_names); 
+make_great(magician_names);
 show_magician(magician_names);
